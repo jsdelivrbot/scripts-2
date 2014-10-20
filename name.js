c1c39576a6359d6bd129a9011aa6eec2f1e94423
Varietyy <chasedss@gmail.com>
2014-10-19 20:13:20 -0400

    Update name.js

--- name.js
+++ name.js
@@ -472,11 +472,3 @@ API.sendChat(""+ data.un +" just gave props to @"+ API.getDJ().username +". :thu
         return null;
         }
 })();
-
-API.on(API.SCORE_UPDATE, voteSkip);
-funnction voteSkip(score){
-var mehSkip = 8;
-if(score.negative >= mehSkip){
-API.moderateForceSkip();
-}
-}
