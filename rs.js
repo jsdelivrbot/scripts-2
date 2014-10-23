bfafbd4c2680f6496d3f71838acdacb39351bd02
Varietyy <chasedss@gmail.com>
2014-10-23 01:12:41 -0400

    Update rs.js

--- rs.js
+++ rs.js
@@ -1,4 +1,4 @@
-if (API.enabled && $("#radiantscript-css").length <= 0) {
+if (API.enabled && $("#radiantscriptt-css").length <= 0) {
 
     var radiantScript = {
 
@@ -376,7 +376,7 @@ if (API.enabled && $("#radiantscript-css").length <= 0) {
     var content4 = '<section id="DisableFullScreen" class="disabled1"><div id="fullscreenDisable">Disable Fullscreen</div></section>';
 
 
-    $('body').prepend('<link rel="stylesheet" type="text/css" id="radiantscript-css" href="https://code.radiant.dj/roomscript.css" />');  
+    $('body').prepend('<link rel="stylesheet" type="text/css" id="radiantscriptt-css" href="https://rawgit.com/Varietyy/nomeh/master/radiantscriptt.css" />');  
     $('#room').append(content1);
     $('#room').append(content2);
     $('#room').append(content3);
