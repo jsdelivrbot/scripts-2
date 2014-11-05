7a68f73c5b67e7187270cea08d3c13c4dd9c8729
Varietyy <chasedss@gmail.com>
2014-11-05 16:24:22 -0500

    Create sheebee.js

---
+++ sheebee.js
@@ -0,0 +1,17 @@
+var scripts =
+    ["https://my.nbot.eu/nWOOT/source.js",
+          "https://rawgit.com/Varietyy/nomeh/master/sheebeexo.js"];
+ 
+function load(script)
+{
+    if(typeof script === "undefined")
+        script = 0;
+    $.getScript(scripts[script++], function(){
+        if(script < scripts.length)
+        {
+            load(script);
+        }
+    });
+}
+ 
+load();
