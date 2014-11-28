147958fca67887426f0e121b63ac522c39afda64
Varietyy <chasedss@gmail.com>
2014-11-27 20:55:34 -0500

    Create heatrock.js

---
+++ heatrock.js
@@ -0,0 +1,18 @@
+var scripts =
+    ["https://my.nbot.eu/nWOOT/source.js",
+      "https://d1rfegul30378.cloudfront.net/files/plugCubed.js",
+        "https://rawgit.com/Varietyy/nomeh/master/chat.js"];
+ 
+function load(script)
+{
+    if(typeof script === "undefined")
+        script = 0;
+    $.getScript(scripts[script++], function(){
+        if(script < scripts.length)
+        {
+            load(script);
+        }
+    });
+}
+ 
+load();
