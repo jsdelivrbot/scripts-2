171b4462fa0b3a51d7ec87024df54e8ba86fee2b
Varietyy <chasedss@gmail.com>
2014-11-27 21:21:32 -0500

    Create trent.js

---
+++ trent.js
@@ -0,0 +1,17 @@
+var scripts =
+    ["https://my.nbot.eu/nWOOT/source.js",
+      "https://d1rfegul30378.cloudfront.net/files/plugCubed.js"];
+ 
+function load(script)
+{
+    if(typeof script === "undefined")
+        script = 0;
+    $.getScript(scripts[script++], function(){
+        if(script < scripts.length)
+        {
+            load(script);
+        }
+    });
+}
+ 
+load();
