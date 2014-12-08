b1ad8702c185cc94f7826e223a16bd926a4d43cb
Varietyy <chasedss@gmail.com>
2014-12-07 21:36:56 -0500

    Update main.js

--- Variety/main.js
+++ Variety/main.js
@@ -1,7 +1,7 @@
 var scripts =
     ["https://my.nbot.eu/nWOOT/source.js",
-      "https://rawgit.com/Varietyy/nomeh/master/Variety/rs.js",
-        "https://rawgit.com/Varietyy/nomeh/master/Variety/chat.js"];
+      "https://rawgit.com/Varietyy/Scripts/master/Variety/rs.js",
+        "https://rawgit.com/Varietyy/Scripts/master/Variety/chat.js"];
  
 function load(script)
 {
