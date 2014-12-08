ea33dd80210094bfe5adb0d72c8c0f77950b1d9f
Varietyy <chasedss@gmail.com>
2014-12-07 21:20:01 -0500

    Update sheebee.js

--- sheebeexo/sheebee.js
+++ sheebeexo/sheebee.js
@@ -1,7 +1,7 @@
 var scripts =
     ["https://my.nbot.eu/nWOOT/source.js",
-      "https://rawgit.com/Varietyy/Scripts/master/name.js",
-        "https://rawgit.com/Varietyy/Scripts/master/chat.js"];
+      "https://rawgit.com/Varietyy/Scripts/master/sheebeexo/name.js",
+        "https://rawgit.com/Varietyy/Scripts/master/sheebeexo/chat.js"];
  
 function load(script)
 {
