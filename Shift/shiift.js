c82e205263926ee342e17a6011402564baca072d
Varietyy <chasedss@gmail.com>
2014-12-07 21:31:38 -0500

    Update shiift.js

--- Shift/shiift.js
+++ Shift/shiift.js
@@ -1,6 +1,6 @@
 var scripts =
     ["https://my.nbot.eu/nWOOT/source.js",
-          "https://rawgit.com/Varietyy/nomeh/master/shiftchat.js"];
+          "https://rawgit.com/Varietyy/nomeh/master/Shift/shiftchat.js"];
  
 function load(script)
 {
