47e76584828ebdaa5b44c1873a9e2c18fc47e0b8
Varietyy <chasedss@gmail.com>
2014-12-07 21:49:42 -0500

    Update and rename sheebeexo/sheebee.js to sheebee.js

--- sheebee.js
+++ sheebee.js
@@ -1,7 +1,7 @@
 var scripts =
     ["https://my.nbot.eu/nWOOT/source.js",
-      "https://rawgit.com/Varietyy/Scripts/master/sheebeexo/name.js",
-        "https://rawgit.com/Varietyy/Scripts/master/sheebeexo/chat.js"];
+      "https://rawgit.com/Varietyy/scripts/master/sheebeexo/name.js",
+        "https://rawgit.com/Varietyy/scripts/master/sheebeexo/chat.js"];
  
 function load(script)
 {
