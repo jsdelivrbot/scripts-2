9bd7013d788b39e2023c8dc2fda4a40252402c52
Varietyy <chasedss@gmail.com>
2015-01-22 17:44:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-var scripts =
-    ["https://my.nbot.eu/nWOOT/source.js",
-       "https://code.radiant.dj/rs.min.js",
-         "https://rawgit.com/Varietyy/scripts/master/chat.js"];
+var scripts = ["https://code.radiant.dj/rs.min.js",
+              "https://rawgit.com/Varietyy/scripts/master/chat.js"];
  
 function load(script)
 {
