940042a2d63d8f1750b01ee1769ba8b2616a8f85
Varietyy <chasedss@gmail.com>
2015-01-24 23:53:36 -0500

    Update sheebee.js

--- sheebee.js
+++ sheebee.js
@@ -1,6 +1,5 @@
 var scripts =
-    ["https://my.nbot.eu/nWOOT/source.js",
-      "https://rawgit.com/Varietyy/scripts/master/name.js",
+    ["https://rawgit.com/Varietyy/scripts/master/name.js",
         "https://rawgit.com/Varietyy/scripts/master/chat.js"];
  
 function load(script)
