525b183f94dbdec8e67306dfc737237e2767ae09
Varietyy <chasedss@gmail.com>
2015-02-15 16:53:27 -0500

    Update chase.js

--- chase.js
+++ chase.js
@@ -22,7 +22,10 @@ var m, msgs;
 msgs = [
  "/me Reminder: Be sure to check the OP list before you play so you don't get skipped: http://pastebin.com/eW26rqFW",
  "/me Reminder: Chat every 60 minutes in order to DJ or you will be removed from the waitlist!",
+ "/me Reminder: The song length limit is 8 minutes!",
+ "/me Reminder: Try to keep your woot/meh ratio above 1.0. (Woot more than meh)",
  "/me Reminder: Ask the staff if you are unsure if your song is appropriate for the room!",
+ "/me Reminder: Don't get butthurt if you lose your Resident DJ; you aren't the only one.",
  "/me Reminder: If you disconnect and you lose your spot, type !dc and the bot will move you back to your position!",
  "/me Reminder: The bot will skip you if you acquire 8 meh's on your track!",
  "/me Reminder: If you don't like a song, mute it and move on.",
