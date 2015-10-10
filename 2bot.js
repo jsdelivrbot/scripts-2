b1718525c1ff8642891dcbfc491d8c0889d90c5f
Varietyy <chasedss@gmail.com>
2015-10-10 12:22:36 -0400

    Update 2bot.js

--- 2bot.js
+++ 2bot.js
@@ -117,6 +117,27 @@ if (typeof trackbot !== "undefined") {
 					msg = "" + msg;
 				break;
 			}
+			/*$("#chat .chat-container .chat-main").append($(
+				'<li class="fcs-'+type+' user-55ffc26f1564a403003f527e">\
+					<div class="stream-item-content">\
+						<div class="chatDelete" onclick="$(this).closest(\'li\').remove();"><span class="icon-close"></span></div>\
+						<div class="image_row">\
+							<img src="https://api.dubtrack.fm/user/55ffc26f1564a403003f527e/image" alt="wibla" onclick="Dubtrack.helpers.displayUser(\'55ffc26f1564a403003f527e\', this);" class="cursor-pointer" onerror="Dubtrack.helpers.image.imageError(this);">\
+						</div>\
+						<div class="activity-row">\
+							<div class="text"><p><a href="#" class="username">[FCS] </a>'+msg+'</p></div>\
+							<div class="meta-info">\
+								<span class="username">WiBla </span>\
+								<i class="icon-dot"></i>\
+								<span class="timeinfo">\
+									<time class="timeago" datetime="undefined" title="Creator\'s website"><a target="_blank"href="http://wibla.free.fr/FCS/">http://wibla.free.fr</a></time>\
+								</span>\
+							</div>\
+						</div>\
+					</div>\
+				</li>'));
+		}
+	};*/
 	if (!localStorage.getItem("trackbot")) {
 		localStorage.setItem("trackbot", JSON.stringify(trackbot.defaultSettings));
 	} else {
